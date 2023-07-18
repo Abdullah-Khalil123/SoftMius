83c1418cf2ee32057daa91f9d84e39d55c76de3a
M-Ali-Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2023-07-18 19:43:19 +0500

    Update mainp.js

--- mainp.js
+++ mainp.js
@@ -14,3 +14,24 @@ window.addEventListener('scroll',function () {
     text.style.marginTop = value * 0.5 + 'px';
     //explore.style.marginTop = value * 0.6 + 'px';
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to handle the scroll event
+    function handleScroll() {
+      const header = document.querySelector('header');
+      const scrollPosition = window.scrollY;
+  
+      // Set the header class based on the scroll position
+      if (scrollPosition > 0) {
+        header.classList.add('solid-header');
+        header.classList.remove('transparent-header');
+      } else {
+        header.classList.remove('solid-header');
+        header.classList.add('transparent-header');
+      }
+    }
+  
+    // Attach the handleScroll function to the scroll event
+    window.addEventListener('scroll', handleScroll);
+  });
+  
